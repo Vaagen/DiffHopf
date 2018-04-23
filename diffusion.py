c920d07783d59d9b8baf2cf7d12ee369f6008d7b
Vaagen <pcthrane@stud.ntnu.no>
2018-04-23 18:56:07 +0200

    Add check for correct setup of BCs in euler_explicit.

---
+++ diffusion.py
@@ -0,0 +1,55 @@
+# diffusion.py
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Handy function
+def pause(message=''):
+    print(message)
+    programPause = raw_input("Press <ENTER> to continue...")
+
+
+def euler_explicit(y,a,T,condition='dirichlet_zero'):
+    ''' Solves dy = a*dy/dx with explicit Euler method.
+    T: number of time steps to take.
+    y: initial state. '''
+    if condition == 'dirichlet_zero':
+        # Completely absorbing boundary condition.
+        # Check that vector is correctly initiated
+        if y[0]!=0 or y[-1]!=0:
+            print("Absorbing Dirichlet BCs need y[0]=y[-1]=0, y was changed accordingly.")
+            y[0]=y[-1]=0
+        for t in range(1,T):
+            u = y
+            for i in range(1,len(y)-1):
+                y[i] = u[i] + a * (u[i+1] - 2*u[i] + u[i-1])
+    return y
+
+
+
+
+
+if __name__ == "__main__":
+    n_points  = 1000
+    n_steps = 10
+    a = 0.27
+
+    x = np.linspace(0,1,n_points+2)
+    y = np.zeros([n_points + 2])
+
+    y[round(len(y)/2)] = 1./n_points
+    # for i in range(0,102):
+    #     y[i]=i
+
+    plt.plot(x,y)
+
+
+    euler_explicit(y,a,n_steps,condition='dirichlet_zero')
+    plt.plot(x,y)
+
+    euler_explicit(y,a,n_steps,condition='dirichlet_zero')
+    plt.plot(x,y)
+
+    euler_explicit(y,a,n_steps*20,condition='dirichlet_zero')
+    plt.plot(x,y)
+    plt.show(block=False)
+    pause('Showing function.')
