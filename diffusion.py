43905e7731c859d385bf942105b3dbe954fc74dc
Vaagen <pcthrane@stud.ntnu.no>
2018-04-24 11:10:04 +0200

    Change to matrix formulation, use Scipy.

--- diffusion.py
+++ diffusion.py
@@ -1,52 +1,48 @@
 # diffusion.py
 import numpy as np
 import matplotlib.pyplot as plt
-import copy
+import scipy
+from scipy import linalg, sparse
+from scipy.sparse import linalg, csr_matrix
 
 # Handy function
 def pause(message=''):
     print(message)
     programPause = raw_input("Press <ENTER> to continue...")
 
-
-def euler_explicit(y,a,T,condition='dirichlet_zero'):
-    ''' Solves dy = a*dy/dx with explicit Euler method.
-    T: number of time steps to take.
-    y: initial state. '''
-    if condition == 'dirichlet_zero':
-        # Completely absorbing boundary condition.
-        # Check that vector is correctly initiated
-        if y[0]!=0 or y[-1]!=0:
-            print("Absorbing Dirichlet BCs need y[0]=y[-1]=0, y was changed accordingly.")
-            y[0]=y[-1]=0
-        for t in range(1,T):
-            u = copy.copy(y) # Inefficient, but need to not get shift of result
-            for i in range(1,len(y)-1):
-                y[i] = u[i] + a * (u[i+1] - 2*u[i] + u[i-1])
+def euler(y,a,T,condition='dirichlet_zero',scheme='implicit'):
+    '''Euler scheme with Au(n+1)=Bu(n).'''
+    if scheme=='implicit':
+        if condition == 'dirichlet_zero':
+            # Completely absorbing boundary condition.
+            A = scipy.sparse.eye(len(y-2),format='csc')
+            B = sparse.diags([a, 1-2*a, a], [-1, 0, 1], shape=(len(y), len(y)), format='csc').toarray()
+    Ainv = sparse.linalg.inv(A)
+    for t in range(1,T):
+        y = Ainv.dot(B.dot(y))
     return y
 
 
 
-
-
 if __name__ == "__main__":
-    n_points  = 500
-    n_steps = 10
+
+    n_points  = 100
+    n_steps = 100
     a = 0.27
 
     x = np.linspace(0,1,n_points+2)
     y = np.zeros([n_points + 2])
 
     y[round(len(y)/2)] = 1./n_points
-    # for i in range(0,102):
-    #     y[i]=i
+    for i in range(0,len(y)):
+        y[i]=i
     plt.plot(x,y)
 
-    euler_explicit(y,a,n_steps,condition='dirichlet_zero')
+    y = euler(y,a,n_steps,condition='dirichlet_zero',scheme='implicit')
     plt.plot(x,y)
-    euler_explicit(y,a,n_steps,condition='dirichlet_zero')
+    y = euler(y,a,n_steps,condition='dirichlet_zero',scheme='implicit')
     plt.plot(x,y)
-    euler_explicit(y,a,n_steps*20,condition='dirichlet_zero')
+    y = euler(y,a,n_steps*10,condition='dirichlet_zero',scheme='implicit')
     plt.plot(x,y)
 
     plt.show(block=False)
